22dbad70fa35a714a6c8b8a187d6601b88708612
CrashOverride <Cr4shOverride@gmx.de>
2019-06-06 13:46:33 +0200

    Add files via upload

---
+++ explain/utilmoneystr/explain_preset.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2017-2019 myneTEC
+
+include <mynetec>
+
+
+-------------------------
+utilmoneystr.h
+-------------------------
+
+short code file, just containing:
+
+Preprocessor statements for marks (we are already used to them)
+
+parse and Format Functions (line 17-19)
+
+----------------------------
+utilstrencodings.cpp
+----------------------------
+
+same here the file is implementing us the needed logic for those 3 functiones mentioned before:
+
+FormatMoney function (line 14-33)
+
+ParseMoney function (line 36-39)
+
+ParseMoney function longer version (line 41-79)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
